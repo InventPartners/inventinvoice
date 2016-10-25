64a9c28432660218191f428ec10f3be7be20a828
Matt Bradley <matt@inventpartners.com>
2016-10-25 14:46:37 +0100

    Removing config

--- config/config.inc.php
+++
@@ -1,1 +0,0 @@
-<?phpdefine('INSTALL_ROOT' , '/var/www/dotadmin/3.0/site/inventinvoice/');define('APP_PATH' , INSTALL_ROOT);define('SITE_DOMAIN' , 'http://inventinvoice.dev:8080');define('SHARED_PATH' , INSTALL_ROOT . 'shared/');define('DB_HOST' , 'localhost');define('DB_NAME' , 'inventinvoice');define('DB_USER' , 'root');define('DB_PASS' , 'root');
